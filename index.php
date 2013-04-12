fa5b03c70be9f7d3b1cc24e5df43d2d3d8ee7a37
F4R0N <mail@fabian1998.de>
2013-04-12 16:43:22 +0200

    Added index.php and style-login.css

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>
+			Die Bündnisse
+		</title>
+		<link href='http://fonts.googleapis.com/css?family=Fondamento' rel='stylesheet' type='text/css'>
+		<link href="style-login.css" rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<header>
+			<h1 class="header center">Die Bündnisse</h1>
+		</header>
+		<article>
+			<section id="login" class="center">
+			    <h3 class="header center">Login</h3>
+				<form action="" method="POST" accept-charset="ISO-ISO-8859-1">
+					Benutzername
+					<input type="text" name="Nickname" maxlength="20">
+					Passwort
+					<input type="password" name="Passwort" placeholder="******">
+					<input type="submit" name="Einloggen" value="Einloggen">
+				</form>
+			</section>
+		</article>
+	</body>
+</html>
