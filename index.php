026e5c01852681d4b126d1a9970107a67d9725f5
Fabian <mail@fabian1998.de>
2013-04-23 16:13:02 +0200

    Another Bugfix 	modified:   index.php 	deleted:    style-login.css

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
-<!DOCTYPE HTML>
-<html>
-        <head>
-            <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
-		<title>
-			Die B&uuml;ndnisse
-		</title>
-		<link href='http://fonts.googleapis.com/css?family=Fondamento' rel='stylesheet' type='text/css'>
-		<link href="style-login.css" rel='stylesheet' type='text/css'>
-	</head>
-	<body>
-		<header>
-			<h1 class="header center">Die B&uuml;ndnisse</h1>
-		</header>
-		<article>
-			<section id="login" class="center">
-			    <h3 class="header center">Login</h3>
-				<form action="" method="POST" accept-charset="ISO-ISO-8859-1">
-					Benutzername
-					<input type="text" name="Nickname" maxlength="20">
-					Passwort
-					<input type="password" name="Passwort" placeholder="******">
-					<input type="submit" name="Einloggen" value="Einloggen">
-				</form>
-			</section>
-		</article>
-	</body>
-</html>
+<?php
+session_start();
+
+include "../private/mysql.php";
+include "includes/login.php";
+include "includes/template.php";
+
+if (isset($_POST['Benutzername']) && isset($_POST['Passwort'])) {
+    $login = new login($_POST['Benutzername'], $_POST['Passwort']);
+    if ($login->login())
+        header('LOCATION: game.php');
+    else
+        $data['login_msg'] = 'Login fehlgeschlagen!';
+}
+
+include_template("header");
+include_template("login", $data);
+include_template("footer");
+
