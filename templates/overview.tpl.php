ddc88dd30c95d768ac5d5089e06b5dba6988379a
Fabian <mail@fabian1998.de>
2013-04-23 17:03:41 +0200

    modified:   templates/overview.tpl.php

--- templates/overview.tpl.php
+++ templates/overview.tpl.php
@@ -2,6 +2,6 @@
     <h1 class="header center">Die B&uuml;ndnisse</h1>
 </header>
 <article>
-    Willkommen im Spiel Nr. <?= $_SESSION['ID']; ?>
+    Willkommen im Spiel Nr. <?= $_SESSION['ID']; ?>, Was willst du tun?
     <a href="?logout=true">Logout</a>
 </article>
