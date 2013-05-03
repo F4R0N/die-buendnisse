04ab6d4014307b520ab4e8651804237d272bfcad
|V1TR0 <xandi98@web.de>
2013-05-04 01:06:29 +0200

    minor fixes and changes

--- templates/overview.tpl.php
+++ templates/overview.tpl.php
@@ -2,6 +2,9 @@
     <h1 class="header center">Die B&uuml;ndnisse</h1>
 </header>
 <div id="main_content">
-    Willkommen im Spiel Nr. <?= $data['player']->ID; ?>, du hast es geschafft! Wir freuen uns,
-    dich in unserer Pre-Pre-Alpha-Version begrussen zu durfen, <?= htmlentities($data['player']->username); ?>.
+    <div id="login_overview_field">
+        Willkommen bei "Die B&uuml;ndnisse", Pre-Pre-Alpha-Tester <?= htmlentities($data['player']->username); ?>!<br /> 
+        Du hast dich auf eine abenteurliche Reise startend in der Steinzeit eingelassen.<br />
+        Wir w&uuml;nschen dir viel Gl&uuml;ck und Erfolg, Nr. <?= $data['player']->ID; ?>!
+    </div>
 </div>
