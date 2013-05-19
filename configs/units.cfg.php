21be34ac2f563c797ae14da3cae2fe9ae63a1130
|V1TR0 <xandi98@web.de>
2013-05-19 20:01:32 +0200

    meta in header

--- configs/units.cfg.php
+++ configs/units.cfg.php
@@ -9,4 +9,12 @@ $units['stone'] = array(
     new unit("Speerwerfer", 5, 10, 8), // Deff
     new unit("Keulenschwinger", 20, 15, 12)  // Off
 );
+
+$units['bronce'] = array(
+    new unit("Braxtkampf", 10, 5, 10), // Off
+    new unit("Bronzeewerfer", 10, 10, 8), // Ausgeglichen
+    new unit("Speertrager", 5, 15, 10), // Deff
+    new unit("Speerwerfer", 5, 10, 8), // Deff
+    new unit("Keulenschwinger", 20, 15, 12)  // Off
+);
 ?>
--- includes/map.inc.php
+++ includes/map.inc.php
@@ -1,3 +1,16 @@
 <?php
-    contain("tpl" ,"map");
+
+
+if(is_numeric($_GET["x"]) && is_numeric($_GET["y"])){
+
+$data["pos"]["x"] = $_GET["x"];
+$data["pos"]["y"] = $_GET["y"];
+
+} else {
+    $data["pos"]["x"] = 0;
+    $data["pos"]["y"] = 0;
+}
+
+contain("tpl" ,"map", $data);
+
 ?>
