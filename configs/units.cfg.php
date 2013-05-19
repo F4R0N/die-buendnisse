1ad688b566b637f5c423d1728a8559090f92b0b0
F4R0N <mail@fabian1998.de>
2013-05-19 19:37:36 +0200

    Map and Unit

--- configs/units.cfg.php
+++ configs/units.cfg.php
@@ -2,11 +2,21 @@
 
 contain("class", "units");
 
-$units = array(
+$units['stone'] = array(
     new unit("Steinaxtkampf", 10, 5, 10), // Off
     new unit("Steinewerfer", 10, 10, 8), // Ausgeglichen
     new unit("Speertrager", 5, 15, 10), // Deff
     new unit("Speerwerfer", 5, 10, 8), // Deff
     new unit("Keulenschwinger", 20, 15, 12)  // Off
 );
+/* ....
+$units['bronce'] = array(
+    new unit("Braxtkampf", 10, 5, 10), // Off
+    new unit("Steinewerfer", 10, 10, 8), // Ausgeglichen
+    new unit("Speertrager", 5, 15, 10), // Deff
+    new unit("Speerwerfer", 5, 10, 8), // Deff
+    new unit("Keulenschwinger", 20, 15, 12)  // Off
+);
+
+ */
 ?>
--- includes/map.inc.php
+++ includes/map.inc.php
@@ -1,5 +1,14 @@
 <?php
 
+if(is_numeric($_GET["x"]) && is_numeric($_GET["y"])){
 
-contain("tpl" ,"map");
+$data["pos"]["x"] = $_GET["x"];
+$data["pos"]["y"] = $_GET["y"];
+
+} else {
+    $data["pos"]["x"] = 0;
+    $data["pos"]["y"] = 0;
+}
+
+contain("tpl" ,"map", $data);
 ?>
